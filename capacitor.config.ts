5865719c8b85714746f00fb91106cb1c87c81bf9
lancunren-byte <lancunren-byte@users.noreply.github.com>
2026-04-03 14:30:38 +0800

    feat: update app name to Ohmee and replace app icon with brand logo

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
   appId: 'com.ohmee.store',
-  appName: 'Ohmee 巡店',
+  appName: 'Ohmee',
   webDir: 'dist',
   server: {
     androidScheme: 'https',
